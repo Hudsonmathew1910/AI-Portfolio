e97ab237502e59fbb914ec2d728fc95b51883b3e
Hudson Mathew <hudsonmathew2004@gmail.com>
2026-02-20 11:25:10 +0530

    Mobile UI_1

--- portfolio_ai/settings.py
+++ portfolio_ai/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-6!bh_i&$g5d74bkfuzjh0b3he^o!bjv_-k+9za58hd$gig-dmk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
